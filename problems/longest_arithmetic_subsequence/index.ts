3527ac1ce0a8c9e2318c2f3c17360f9121e0571d
Eric Ventor <ericventor97@gmail.com>
2023-03-02 15:08:40 -0800

    linting

--- problems/longest_arithmetic_subsequence/index.ts
+++ problems/longest_arithmetic_subsequence/index.ts
@@ -1,22 +1,22 @@
-const example1 = [83,20,17,43,52,78,68,45];
+const example1 = [83, 20, 17, 43, 52, 78, 68, 45];
 
 function longestArithSeqLength(nums: number[]): number {
   let maxDifference = -1;
 
   const runThroughs: { [x: string]: { [x: string]: number } } = {};
-  for (let i = 0; i < nums.length; i += 1){
-    let curNum = nums[i];
+  for (let i = 0; i < nums.length; i += 1) {
+    const curNum = nums[i];
     runThroughs[i] = {};
-    
-    for (let j = i + 1; j < nums.length; j += 1){
+
+    for (let j = i + 1; j < nums.length; j += 1) {
       const subNum = nums[j];
       const difference = curNum - subNum;
       runThroughs[i][difference] = (runThroughs[i][difference] ?? 1) + 1;
     }
     maxDifference = Math.max(...Object.values(runThroughs[i]));
   }
-    
+
   return maxDifference;
-};
+}
 
 console.log(longestArithSeqLength(example1));
--- problems/substring_xor_queries/index.ts
+++ problems/substring_xor_queries/index.ts
@@ -4,43 +4,43 @@ function substringXorQueries(s: string, queries: number[][]): number[][] {
   let maxNum = -Infinity;
 
   // hashmap {necessaryDecVal: [queriesIndex]}
-  for(let i = 0; i < queries.length; i += 1) {
-      const [firstI, secondI] = queries[i];
-      const necessaryDecVal = bitwise(firstI, secondI);
+  for (let i = 0; i < queries.length; i += 1) {
+    const [firstI, secondI] = queries[i];
+    const necessaryDecVal = bitwise(firstI, secondI);
 
-      maxNum = Math.max(necessaryDecVal, maxNum);
+    maxNum = Math.max(necessaryDecVal, maxNum);
 
-      if (necessaryVals[necessaryDecVal]) {
-          necessaryVals[necessaryDecVal].push(i);
-      } else {
-          necessaryVals[necessaryDecVal] = [i];
-      }
+    if (necessaryVals[necessaryDecVal]) {
+      necessaryVals[necessaryDecVal].push(i);
+    } else {
+      necessaryVals[necessaryDecVal] = [i];
+    }
   }
 
   for (let i = 0; i < s.length; i += 1) {
-      for (let j = i; j < s.length; j += 1) {
-          const binarySubStr = s.substring(i,j+1);
-          const decVal = convertBinaryToDecimal(binarySubStr);
+    for (let j = i; j < s.length; j += 1) {
+      const binarySubStr = s.substring(i, j + 1);
+      const decVal = convertBinaryToDecimal(binarySubStr);
 
-          if (decVal > maxNum || (j - i) > 31){
-              break;
-          }
-
-          if (necessaryVals[decVal]){
-              const queryIndexes = necessaryVals[decVal];
-              queryIndexes.forEach(index => {
-                  if(
-                      queryResponses[index].toString() === [-1,-1].toString() || 
-                      (queryResponses[index][1] - queryResponses[index][0] + 1) > binarySubStr.length
-                  ) {
-                      queryResponses[index] = [i,j];    
-                  }
-              });
-          }
+      if (decVal > maxNum || (j - i) > 31) {
+        break;
       }
+
+      if (necessaryVals[decVal]) {
+        const queryIndexes = necessaryVals[decVal];
+        queryIndexes.forEach((index) => {
+          if (
+            queryResponses[index].toString() === [-1, -1].toString()
+                      || (queryResponses[index][1] - queryResponses[index][0] + 1) > binarySubStr.length
+          ) {
+            queryResponses[index] = [i, j];
+          }
+        });
+      }
+    }
   }
   return queryResponses;
-};
+}
 
 function bitwise(val1: number, val2: number): number {
   return val1 ^ val2;
