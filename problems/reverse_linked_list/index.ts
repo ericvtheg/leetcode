548ed6cb833b20343dd98f4d7cc2517fd15a2828
Eric Ventor <ericventor97@gmail.com>
2023-03-05 21:53:30 -0800

    adding more solutions

--- problems/reverse_linked_list/index.ts
+++ problems/reverse_linked_list/index.ts
@@ -24,3 +24,33 @@ function reverseList(head: ListNode | null): ListNode | null {
 
   return newHead;
 }
+
+// iterative - in place
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  if (!head.next) return head;
+
+  let prev = null;
+
+  while (head) {
+    const temp = head.next;
+    head.next = prev;
+    prev = head;
+
+    head = temp;
+  }
+
+  return prev;
+}
+
+// recursive in place
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  if (!head.next) return head;
+
+  const node = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+
+  return node;
+}
