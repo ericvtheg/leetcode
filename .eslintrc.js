a67e18c2fc45acf4c5853bfb2c6fe2423700a484
Eric Ventor <ericventor97@gmail.com>
2023-03-28 21:03:55 -0700

    linting config updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-unused-vars': ['off'],
+    'no-bitwise': ['off'],
+    'no-plusplus': ['off'],
+    'no-restricted-globals': ['off'],
+    'no-use-before-define': ['off'],
+    'no-param-reassign': ['off'],
+  },
 };
