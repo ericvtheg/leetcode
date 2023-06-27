8415171dc92289816f69d4646782815e70f70a18
Eric Ventor <ericvtheg@Erics-MBP.lan>
2023-06-26 23:15:59 -0700

    linting

--- problems/valid_palindrome/index.ts
+++ problems/valid_palindrome/index.ts
@@ -1,25 +1,25 @@
 function isLetter(char: string) {
-    return (((char.toUpperCase() != char.toLowerCase()) || Number.isInteger(+char)) && char != ' ');
+  return (((char.toUpperCase() !== char.toLowerCase()) || Number.isInteger(+char)) && char !== ' ');
 }
 
 function isPalindrome(s: string): boolean {
-    let newString = [];
-    for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-        if (isLetter(char)) {
-            newString.push(char.toLowerCase());
-        }
+  const newString = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (isLetter(char)) {
+      newString.push(char.toLowerCase());
     }
+  }
 
-    let end = newString.length - 1;
-    let start = 0;
-    while (start <= end) {
-        if (newString[start] !== newString[end]) {
-            return false;
-        }
-        start += 1;
-        end -= 1;
+  let end = newString.length - 1;
+  let start = 0;
+  while (start <= end) {
+    if (newString[start] !== newString[end]) {
+      return false;
     }
+    start += 1;
+    end -= 1;
+  }
 
-    return true;
-};
+  return true;
+}
