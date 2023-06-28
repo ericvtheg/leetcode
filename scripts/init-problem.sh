d70954cd89d80b18088526eef6065a72b425ee3f
Eric Ventor <ericventor97@gmail.com>
2023-06-27 18:21:41 -0700

    added init package json script

--- scripts/init-problem.sh
+++ scripts/init-problem.sh
@@ -5,7 +5,22 @@ if [[ $# -eq 0 ]]; then
   exit 1
 fi
 
-folder_name=$1
+folder_name=""
+difficulty=""
+
+for arg in "$@"; do
+  if [[ -z "$folder_name" ]]; then
+    folder_name=$arg
+  else
+    difficulty=$arg
+  fi
+done
+
+if [[ -z "$folder_name" ]]; then
+  echo "Please provide a folder name."
+  exit 1
+fi
+
 folder_path="./problems/$folder_name"
 
 # Create the folder if it doesn't exist
@@ -19,10 +34,27 @@ touch "$index_file"
 readme_file="$folder_path/README.md"
 touch "$readme_file"
 
+# Determine difficulty level
+case $difficulty in
+  easy|EASY|Easy)
+    difficulty="🟢Easy🟢 \\"
+    ;;
+  medium|MEDIUM|Medium)
+    difficulty="🟡Medium🟡 \\"
+    ;;
+  hard|HARD|Hard)
+    difficulty="🔴Hard🔴 \\"
+    ;;
+  *)
+    echo "Invalid difficulty level specified. Defaulting to Easy."
+    difficulty="🟢Easy🟢 \\"
+    ;;
+esac
+
 # Add contents to the README.md file
 echo "# $folder_name" >> "$readme_file"
 echo >> "$readme_file"
-echo "Difficulty: 🟢Easy🟢 \\" >> "$readme_file"
+echo "Difficulty: $difficulty" >> "$readme_file"
 echo "Completed: [✅] \\" >> "$readme_file"
 echo "Link: /$folder_name/ \\" >> "$readme_file"
 
