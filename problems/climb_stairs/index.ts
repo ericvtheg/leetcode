8a4fda28b0c8fd44076bb463367318b29d34632a
Eric Ventor <ericventor97@gmail.com>
2024-01-17 18:21:16 -0800

    improved space complexity of climb stairs

--- problems/climb_stairs/index.ts
+++ problems/climb_stairs/index.ts
@@ -1,11 +1,17 @@
 function climbStairs(n: number): number {
-  if (n <= 2) return n;
-
-  const dp = new Array(n);
-  dp[0] = 1;
-  dp[1] = 2;
-  for (let i = 3; i <= n; i++) {
-    dp[i - 1] = dp[i - 2] + dp[i - 3];
+  if (n <= 3) {
+    return n;
   }
-  return dp[n - 1];
+
+  let prevPrev = 1;
+  let prev = 2;
+  let sum;
+
+  for (let i = 2; i < n; i += 1) {
+    sum = prevPrev + prev;
+    prevPrev = prev;
+    prev = sum;
+  }
+
+  return sum;
 }
