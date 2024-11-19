cced94750fcc8ddcef7a5c19ceda94b9a58c25f4
Eric Ventor <ericventor97@gmail.com>
2024-11-18 16:15:33 -0800

    completed linked list cycle

---
+++ problems/linked_list_cycle/README.md
@@ -0,0 +1,9 @@
+# 141. Linked List Cycle
+
+Difficulty: 🟢Easy🟢 \
+Completed: [✅] \
+Link: https://leetcode.com/problems/linked-list-cycle \
+
+## Notes
+
+Floyd's algorithm garantees that slow and fast will meet by the time slow completes the cycle. This results in an O(n + k) where k is the length of the cycle.
---
+++ problems/linked_list_cycle/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  let fast = head;
+  let slow = head;
+  while (fast) {
+    fast = fast.next?.next;
+    slow = slow.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
