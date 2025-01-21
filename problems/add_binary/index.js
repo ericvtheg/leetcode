e4bf49b3222452f37b7c607a6b8f5085df9a3f5d
Eric Ventor <ericventor97@gmail.com>
2025-01-20 21:40:29 -0800

    completed add binary

--- problems/add_binary/index.js
+++ problems/add_binary/index.js
@@ -47,3 +47,37 @@ var addBinary = function (a, b) {
 
   return res;
 };
+
+//take 2
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  let result = '';
+  let carry = 0;
+
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (j >= 0 || i >= 0 || carry > 0) {
+    const aNum = parseInt(a[i] ?? '0');
+    const bNum = parseInt(b[j] ?? '0');
+
+    let num = aNum + bNum + carry;
+    carry = 0;
+
+    if (num >= 2) {
+      carry = 1;
+      num -= 2;
+    }
+
+    result = `${num}` + result;
+
+    j -= 1;
+    i -= 1;
+  }
+
+  return result;
+};
